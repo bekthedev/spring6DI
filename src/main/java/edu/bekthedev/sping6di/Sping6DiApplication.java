de482b2a8eb1999b57d3393e4232812192f1597f
Bek Johansson <beksaylor@gmail.com>
2025-04-17 04:33:52 -0500

    first commit

---
+++ src/main/java/edu/bekthedev/sping6di/Sping6DiApplication.java
@@ -0,0 +1,13 @@
+package edu.bekthedev.sping6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sping6DiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sping6DiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/edu/bekthedev/sping6di/Sping6DiApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.bekthedev.sping6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sping6DiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
